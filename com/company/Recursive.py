7af6b4b984d82ce850acef72620d0d1a452c9292
Aniket Chaudhari <chaudharianiket379@gmail.com>
2024-06-08 12:13:09 +0530

    Update README.md

--- com/company/Recursive.py
+++ com/company/Recursive.py
@@ -4,12 +4,14 @@ def Print(n):
     if n == 0:
         return
     print(n)
-    return Print(n-1)
+    return Print(n - 1)
+
 
 n = int(input("Enter the number: "))
 output = Print(n)
 
 print("-------------------------------------")
+
 
 # sum from 1 to N
 
@@ -17,15 +19,16 @@ def sum_1_To_N(num):
     if num == 1:
         return 1
 
-    ans = num + sum_1_To_N(num-1)
+    ans = num + sum_1_To_N(num - 1)
     return ans
 
 
 n = int(input("Enter the number: "))
 output1 = sum_1_To_N(n)
-print("The sum from 1 to N is: ",output1)
+print("The sum from 1 to N is: ", output1)
 
 print("-------------------------------------")
+
 
 # Print from 1 to N
 
@@ -35,16 +38,19 @@ def Print_1_To_N(n):
     Print_1_To_N(n - 1)
     print(n)
 
+
 n = int(input("Enter the number: "))
 Print_1_To_N(n)
 
 print("-------------------------------------")
 
-def fibonacci(Num, a = 0, b = 1):
+
+def fibonacci(Num, a=0, b=1):
     if Num > 0:
         print(a, end=" ")
 
-        fibonacci(Num-1, b, a+b)
+        fibonacci(Num - 1, b, a + b)
+
 
 n = int(input("Enter the number: "))
 fibonacci(n)
@@ -52,13 +58,15 @@ print()
 
 print("-------------------------------------")
 
+
 # Factor for number
-def Factor_for_number(number, i = 1):
+def Factor_for_number(number, i=1):
     if i > number:
         return
     if n % i == 0:
         print(i, end=" ")
-    Factor_for_number(n, i+1)
+    Factor_for_number(n, i + 1)
+
 
 n = int(input("Enter the number: "))
 Factor_for_number(n)
@@ -67,12 +75,14 @@ print()
 
 print("-------------------------------------")
 
+
 def power(a, b):
     if b == 0:
         return 1
 
-    ans = a * power(a,b-1)
+    ans = a * power(a, b - 1)
     return ans
+
 
 a = int(input("Enter the a: "))
 b = int(input("Enter the b: "))
@@ -80,6 +90,7 @@ res = power(a, b)
 print(res)
 
 print("-------------------------------------")
+
 
 def fib(num):
     if num == 1:
@@ -89,10 +100,10 @@ def fib(num):
     else:
         return fib(num - 1) + fib(num - 2)
 
+
 fib_num = int(input("Enter the number for fib: "))
 # nn = fib(fib_num)
 # print(nn)
 # for list
-for i in range(1, n+1):
+for i in range(1, n + 1):
     print(i)
-
